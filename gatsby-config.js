667d32fb7cc56e938914e6c2bcd04b663f63b9d3
sulemansarwar10 <sulemansarwar10@gmail.com>
2022-01-14 21:46:27 +0500

    build simple web using typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,13 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
-    ]
+      {
+        resolve: `gatsby-plugin-typescript`,
+        options: {
+          isTSX: true, // defaults to false
+          jsxPragma: `jsx`, // defaults to "React"
+          allExtensions: true, // defaults to false
+        },
+      },
+    ],
 }
