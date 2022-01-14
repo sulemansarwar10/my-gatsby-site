ffe8bde6a26fc06ece82dc1e03a8655c85d89d6b
sulemansarwar10 <sulemansarwar10@gmail.com>
2022-01-14 21:57:27 +0500

    create new page
    http://localhost:8000/contact

---
+++ src/pages/contact.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Nav from './nav'
+
+export default function contact() {
+    return (
+        <div>
+            <Nav />
+            <h1> hello from contact page</h1>
+        </div>
+    )
+}
