d0594ac4e38397c18bdf9ec05b3a81e5e4d1da7e
sulemansarwar10 <sulemansarwar10@gmail.com>
2022-01-14 22:25:21 +0500

    ADD Navbar to pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 
 import React from 'react';
-
+import Nav from './nav';
 export default () => {
-    return <div>Hello world from Suleman!</div>;
+    return <div>
+        <Nav />
+        <h1>
+            Hello world from Suleman!
+        </h1>
+    </div>;
 }
